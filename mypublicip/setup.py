912d3a463863083cbc3618d45dcbea7b32f5be59
Jeremie Rouzet <jerems@jeyriku.net>
2026-05-12 22:26:37 +0200

    Bump release version for Nexus

--- mypublicip/setup.py
+++ mypublicip/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mypublicip',
-    version='0.1.0',
+    version='0.1.1',
     author='Jeremie Rouzet',
     author_email='your_email@example.com',
     description='A module to retrieve the public IP address of the current connection.',
