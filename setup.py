0787dab6674a263e718f08cfaa175f533ef889a2
Jeremie Rouzet <jerems@jeyriku.net>
2026-09-15 21:47:04 +0200

    Complete version 0.1.9 and CLI configuration

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='mypublicip',
-    version='0.1.1',
+    version='0.1.9',
     author='Jeremie Rouzet',
-    author_email='your_email@example.com',
+    author_email='jeremie.rouzet@netalps.fr',
     description='A module to retrieve the public IP address of the current connection.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/yourusername/mypublicip',  # Replace with your actual URL
-    packages=find_packages(),
+    packages=['mypublicip'],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',  # Replace with your actual license
@@ -21,7 +21,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'mypublicip=mypublicip.mypublicip:main',
+            'mypublicip=mypublicip.cli:main',
         ],
     },
 )
